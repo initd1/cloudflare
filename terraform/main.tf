2d82f15a9cbc096992ec7cbe04617d28b229c3c6
kash <initd1@outlook.com>
2024-10-02 00:40:08 +0800

    post terraform init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,5 @@
-provider "cloudflare" {
-    api_token = var.cloudflare_api_token
-}
-
 resource "cloudflare_waf_rule" "waf_rule" {
-    zone_id = var.zone_id
+  zone_id = var.zone_id
   filter {
     expression  = var.waf_expression
     paused      = false
